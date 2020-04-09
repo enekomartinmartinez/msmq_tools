17095916fc5233bc7061686e2fd50eddd27cd019
Eneko Martin Martinez <eneko.martin.martinez@gmail.com>
2020-04-09 13:23:20 +0200

    Occigen conf

--- msqg_tools/__init__.py
+++ msqg_tools/__init__.py
@@ -1,3 +1,4 @@
+from msqg_tools.breakds import breakds
 from msqg_tools.inter import int_main
 from msqg_tools.densities import den_main 
 from msqg_tools.mask import mask_main
--- submit.sh
+++ submit.sh
@@ -6,6 +6,6 @@ PATHMT=$SCRATCHDIR/CAL1/msmq_tools
 
 for CONF in $CONFS
 do
-  sbatch -J $CONF $PATHMT/run.sh $PATHMT/break_saltem.py $PATHMT/$CONF
+  sbatch -J $CONF $PATHMT/run.sh $PATHMT/break_saltem.py $PATHMT/${CONF}_param.py
 done
 
